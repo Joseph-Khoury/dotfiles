fde67b8db4bb0b467eead9218669128339a2e877
Joseph-Khoury <me@josephkhoury.dev>
2026-06-19 18:25:46 +0200

    updated the glazewm config to improve keybind parity with aerospace and removed extra files

--- common/nvim/lua/mystuffs/remap.lua
+++ common/nvim/lua/mystuffs/remap.lua
@@ -4,7 +4,7 @@ vim.keymap.set("n", "<leader>pv", '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle Fi
 -- Telescope keybinds ##################################################################
 local builtin = require('telescope.builtin')
 vim.keymap.set("n", '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
--- vim.keymap.set("n", '<C-p>', builtin.git_files, { desc = 'Telescope find files in git' })
+vim.keymap.set("n", '<C-p>', builtin.git_files, { desc = 'Telescope find files in git' })
 vim.keymap.set("n", '<leader>ps', builtin.live_grep, { desc = 'Telescope live grep' })
 
 -- Neoclip keybinds ###################################################################
