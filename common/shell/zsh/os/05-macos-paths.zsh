8299d3385257d77182286a79f910bafe453d18f8
Joseph-Khoury <me@josephkhoury.dev>
2026-06-29 03:16:39 +0200

    Added comments to macos paths

--- common/shell/zsh/os/05-macos-paths.zsh
+++ common/shell/zsh/os/05-macos-paths.zsh
@@ -11,8 +11,10 @@ fi
 # Added by LM Studio CLI (lms)
 export PATH="$PATH:/Users/joe8922/.cache/lm-studio/bin"
 
+# Go
 export PATH=$PATH:$HOME/go/bin
 
+# ??
 export NVM_DIR="${HOME}/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
