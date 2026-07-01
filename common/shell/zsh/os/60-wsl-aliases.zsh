7e325f73cf255370767dea370a736c9be2e70f7a
Joseph-Khoury <me@josephkhoury.dev>
2026-07-01 10:46:36 +0200

    Update

--- common/shell/zsh/os/60-wsl-aliases.zsh
+++ common/shell/zsh/os/60-wsl-aliases.zsh
@@ -3,6 +3,8 @@
 alias explorer='explorer.exe'
 alias openhere='explorer.exe .'
 alias copy='clip.exe'
+alias neovide='/mnt/c/Program\ Files/Neovide/neovide.exe --wsl'
+alias nv='neovide'
 
 if [[ -d /mnt/c/Users ]]; then
   alias cusers='cd /mnt/c/Users'
