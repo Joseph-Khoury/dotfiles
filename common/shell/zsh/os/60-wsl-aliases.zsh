915e16188a5a48c4bf417c86f07a7f48420b5ae3
Joseph-Khoury <me@josephkhoury.dev>
2026-07-01 13:03:50 +0200

    Added Powershell dotfiles zsh aliases
    psdots opens the windows dotfiles directory and psdot (singular) opens
    it in neovide and sets the path

--- common/shell/zsh/os/60-wsl-aliases.zsh
+++ common/shell/zsh/os/60-wsl-aliases.zsh
@@ -5,6 +5,8 @@ alias openhere='explorer.exe .'
 alias copy='clip.exe'
 alias neovide='/mnt/c/Program\ Files/Neovide/neovide.exe --wsl'
 alias nv='neovide'
+alias psdots='cd /mnt/c/Users/joe89/.dotfiles'
+alias psdot='psdots && nvim .'
 
 if [[ -d /mnt/c/Users ]]; then
   alias cusers='cd /mnt/c/Users'
