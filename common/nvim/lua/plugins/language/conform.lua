6207fd9a4c6143b0a7c99e38b3c30d5c0d8659d7
Joseph-Khoury <me@josephkhoury.dev>
2026-08-01 20:35:11 +0200

    Added conform.nvim

---
+++ common/nvim/lua/plugins/language/conform.lua
@@ -0,0 +1,61 @@
+return {
+  "stevearc/conform.nvim",
+
+  event = {
+    "BufReadPre",
+    "BufNewFile",
+  },
+
+  keys = {
+    {
+      "<leader>f",
+      function()
+        require("conform").format({
+          async = false,
+          lsp_format = "fallback",
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Format buffer or selection",
+    },
+  },
+
+  config = function()
+    local conform = require("conform")
+
+    conform.formatters.verible = {
+      append_args = {
+        -- lowRISC basic formatting.
+        "--indentation_spaces=2",
+        "--wrap_spaces=4",
+        "--column_limit=100",
+
+        -- Align parameter and port declarations.
+        "--formal_parameters_alignment=align",
+        "--port_declarations_alignment=align",
+
+        -- Align internal signal declarations.
+        "--module_net_variable_alignment=align",
+
+        -- Align module instantiations.
+        "--named_parameter_alignment=align",
+        "--named_port_alignment=align",
+
+        -- Treat blank lines as separate alignment tables.
+        "--alignment_group_boundary=blank-lines",
+      },
+    }
+
+    conform.setup({
+      formatters_by_ft = {
+        systemverilog = { "verible" },
+        verilog = { "verible" },
+      },
+
+      format_on_save = {
+        timeout_ms = 2000,
+        lsp_format = "fallback",
+      },
+    })
+  end,
+}
