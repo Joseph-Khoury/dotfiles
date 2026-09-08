be67245e9d88b3d87e2c32d84039a0ee9a73194d
Joseph-Khoury <me@josephkhoury.dev>
2026-09-08 18:06:47 +0200

    lazylock, keymaps, and dictionary updates
    I added keybindings for all the numbers for harpoon

--- common/nvim/lua/config/keymaps/plugins.lua
+++ common/nvim/lua/config/keymaps/plugins.lua
@@ -37,10 +37,16 @@ map("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon fil
 map("n", "<leader>hf", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Open harpoon menu" })
 
 -- Go to specific harpoon
-map("n", "<leader>h1", function() harpoon:list():select(1) end, { desc = "Harpoon 1" })
-map("n", "<leader>h2", function() harpoon:list():select(2) end, { desc = "Harpoon 2" })
-map("n", "<leader>h3", function() harpoon:list():select(3) end, { desc = "Harpoon 3" })
-map("n", "<leader>h4", function() harpoon:list():select(4) end, { desc = "Harpoon 4" })
+map("n", "<leader>h1", function() harpoon:list():select(1 ) end, { desc = "Harpoon 1"  })
+map("n", "<leader>h2", function() harpoon:list():select(2 ) end, { desc = "Harpoon 2"  })
+map("n", "<leader>h3", function() harpoon:list():select(3 ) end, { desc = "Harpoon 3"  })
+map("n", "<leader>h4", function() harpoon:list():select(4 ) end, { desc = "Harpoon 4"  })
+map("n", "<leader>h5", function() harpoon:list():select(5 ) end, { desc = "Harpoon 5"  })
+map("n", "<leader>h6", function() harpoon:list():select(6 ) end, { desc = "Harpoon 6"  })
+map("n", "<leader>h7", function() harpoon:list():select(7 ) end, { desc = "Harpoon 7"  })
+map("n", "<leader>h8", function() harpoon:list():select(8 ) end, { desc = "Harpoon 8"  })
+map("n", "<leader>h9", function() harpoon:list():select(9 ) end, { desc = "Harpoon 9"  })
+map("n", "<leader>h0", function() harpoon:list():select(10) end, { desc = "Harpoon 10" })
 
 -- Toggle previous & next buffers stored within Harpoon list
 map("n", "<leader>hp", function() harpoon:list():prev() end, { desc = "Previous harpoon" })
